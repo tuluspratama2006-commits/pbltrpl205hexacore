332dcfa6cec12cd8a651addb70a84cfaa5e85df0
tuluspratama2006-commits <tuluspratama2006@gmail.com>
2026-04-17 18:51:36 +0700

    #

--- app/Models/Service.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Service extends Model {
-    protected $fillable = ['title','description','image','icon','order','is_active'];
-}
