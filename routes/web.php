658104807b8f0891fbfb4808a0d5082d93be06a0
tuluspratama2006-commits <tuluspratama2006@gmail.com>
2026-04-17 19:17:02 +0700

    Fix Home

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-// <<<<<<< HEAD
-// Route::get('/', [HomeController::class, 'index'])->name('home');
-// // =======
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// >>>>>>> 332dcfa6cec12cd8a651addb70a84cfaa5e85df0
+Route::get('/', function () {
+    return view('home');
+});
 
 Route::get('/home', function () {
     return view('home');
