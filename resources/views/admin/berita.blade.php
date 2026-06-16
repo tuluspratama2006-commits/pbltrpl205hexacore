3470b50bf44596cf4f458c14bb7cf898ffd7e331
ahmaddinar02 <dinarahmadnaufal@gmail.com>
2026-06-16 13:58:16 +0700

    hapus filter katergori admin.berita

--- resources/views/admin/berita.blade.php
+++ resources/views/admin/berita.blade.php
@@ -73,17 +73,6 @@
 <div class="table-toolbar">
     <div class="toolbar-left">
         <div class="filter-dropdown">
-            <select id="filterKategori" onchange="filterTable()">
-                <option value="">Semua kategori</option>
-                <option value="Engineering">Engineering</option>
-                <option value="Infrastruktur">Infrastruktur</option>
-                <option value="Konstruksi">Konstruksi</option>
-            </select>
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
-                <polyline points="6 9 12 15 18 9"/>
-            </svg>
-        </div>
-        <div class="filter-dropdown">
             <select id="filterStatus" onchange="filterTable()">
                 <option value="">Status</option>
                 <option value="publish">Publish</option>
