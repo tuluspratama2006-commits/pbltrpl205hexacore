f9513507708aa07483720923fef0a3b6e4a7be76
ahmaddinar02 <dinarahmadnaufal@gmail.com>
2026-06-22 00:12:33 +0700

    web php

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,11 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 // =========================
 Route::prefix('admin')->name('admin.')->middleware('admin')->group(function () {
 
+    Route::get('/dashboard', fn() => view('admin.dashboard'))->name('dashboard');
+    Route::get('/portofolio', fn() => view('admin.portofolio'))->name('portofolio');
+    Route::get('/berita', fn() => view('admin.berita'))->name('berita');
+    Route::get('/testimoni', fn() => view('admin.testimoni'))->name('testimoni');
+
     // Dashboard Admin
     Route::get('/dashboard', [BeritaController::class, 'index'])->name('dashboard');
 
